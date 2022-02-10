597a251388e4e3e9756fd8749682c67ce9e40364
Lemur <danilo_jevtovic@hotmail.com>
2022-02-10 12:03:20 +0100

    inicijalni projekat

---
+++ dictionary2/src/main/java/com/dictionary2/dictionary2/Dictionary2Application.java
@@ -0,0 +1,13 @@
+package com.dictionary2.dictionary2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dictionary2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dictionary2Application.class, args);
+	}
+
+}
