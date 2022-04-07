7940f57652964d72620e6bc31e147dc58e6ac813
lemur <danilo_jevtovic@hotmail.com>
2022-04-08 00:35:54 +0200

    izmjene u kontroleru za recenice

--- dictionary2/src/main/java/com/dictionary2/dictionary2/controller/SentenceController.java
+++ dictionary2/src/main/java/com/dictionary2/dictionary2/controller/SentenceController.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,12 +21,17 @@ public class SentenceController extends CrudController<Sentence> {
 	private SentenceService sentenceService;
 
 	@GetMapping("/dic/{id}")
-	public Page<Sentence> getWordsForDic(@PathVariable String id, @PageableDefault(size = 1) Pageable pageable) {
+	public Page<Sentence> getWordsForDic(@PathVariable String id, @PageableDefault(size = 30) Pageable pageable) {
 		return sentenceService.getSentencesForDic(id, pageable);
 	}
 
 	@GetMapping("/sg/{id}")
-	public Page<Sentence> getWordsForWg(@PathVariable String id, @PageableDefault(size = 1) Pageable pageable) {
-		return sentenceService.getSentencesForDic(id, pageable);
+	public Page<Sentence> getWordsForSg(@PathVariable String id, @PageableDefault(size = 30) Pageable pageable) {
+		return sentenceService.getSentencesForGroup(id, pageable);
+	}
+	
+	@PatchMapping("/{id}/favorite/{value}")
+	public void updateFav(@PathVariable String id, @PathVariable Boolean value) {
+		sentenceService.updateFav(id, value);
 	}
 }
