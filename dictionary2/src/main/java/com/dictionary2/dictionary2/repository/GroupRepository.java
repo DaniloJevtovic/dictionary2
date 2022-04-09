63b70147372510cd32b5e213272bb8e693b5aa86
lemur <danilo_jevtovic@hotmail.com>
2022-04-09 21:52:11 +0200

    dobavljanje grupa za rjecnik

--- dictionary2/src/main/java/com/dictionary2/dictionary2/repository/GroupRepository.java
+++ dictionary2/src/main/java/com/dictionary2/dictionary2/repository/GroupRepository.java
@@ -8,6 +8,8 @@ import com.dictionary2.dictionary2.model.Group;
 
 public interface GroupRepository extends MongoRepository<Group, String> {
 
+	List<Group> findByDicId(String id);
+
 	List<Group> findByDicIdAndType(String id, String type);
 
 }
