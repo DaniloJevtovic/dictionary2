f4e86d463b094fc1d60fe2ce0eeab252916c8efa
lemur <danilo_jevtovic@hotmail.com>
2022-04-10 18:47:28 +0200

    enumeracija za tip rjeci

---
+++ dictionary2/src/main/java/com/dictionary2/dictionary2/model/WordType.java
@@ -0,0 +1,7 @@
+package com.dictionary2.dictionary2.model;
+
+public enum WordType {
+
+	// imenica, zamjenica, pridjev, glagol, prilog, prijedlog, veznik
+	NOUN, PRONOUN, ADJECTIVE, VERB, ADVERB, PREPOSITION, CONJUNCTION, INTERJECTION	
+}
