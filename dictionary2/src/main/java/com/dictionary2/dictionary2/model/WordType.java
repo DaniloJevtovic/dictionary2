be8135721cc55346d06da062b0ac7d3fdf50a491
lemur <danilo_jevtovic@hotmail.com>
2022-04-17 14:23:02 +0200

    izmjene u tipovima rjeci

--- dictionary2/src/main/java/com/dictionary2/dictionary2/model/WordType.java
+++ dictionary2/src/main/java/com/dictionary2/dictionary2/model/WordType.java
@@ -3,5 +3,14 @@ package com.dictionary2.dictionary2.model;
 public enum WordType {
 
 	// imenica, zamjenica, pridjev, glagol, prilog, prijedlog, veznik
-	NOUN, PRONOUN, ADJECTIVE, VERB, ADVERB, PREPOSITION, CONJUNCTION, INTERJECTION	
+	NOUN, PRONOUN, ADJECTIVE, VERB, ADVERB, PREPOSITION, CONJUNCTION, INTERJECTION;
+
+//	NOUN("red"), PRONOUN("orange"), ADJECTIVE("green"), VERB("yellow"), ADVERB("cyan"), PREPOSITION("purple"),
+//	CONJUNCTION("pink"), INTERJECTION("blue");
+//
+//	private String color;
+//
+//	private WordType(String color) {
+//		this.color = color;
+//	}
 }
