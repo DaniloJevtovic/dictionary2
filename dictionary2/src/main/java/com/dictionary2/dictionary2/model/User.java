f3843e61889b13f022de648125b038cf3f2b298d
lemur <danilo_jevtovic@hotmail.com>
2022-07-07 15:26:20 +0200

    prosiren model korisnika

--- dictionary2/src/main/java/com/dictionary2/dictionary2/model/User.java
+++ dictionary2/src/main/java/com/dictionary2/dictionary2/model/User.java
@@ -1,22 +1,67 @@
 package com.dictionary2.dictionary2.model;
 
+import java.sql.Timestamp;
+import java.util.Collection;
+import java.util.Collections;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import lombok.Data;
 
 @Document
 @Data
-public class User {
-	
+public class User implements UserDetails {
+
+	private static final long serialVersionUID = 1L;
+
 	@Id
 	private String id;
 
 	private String firstName;
-	
+
 	private String lastName;
-	
+
 	private String email;
-	
+
 	private String password;
+
+	private String language;
+
+	private String role;
+
+	private Timestamp lastPasswordResetDate;
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return Collections.singleton(new SimpleGrantedAuthority(this.role));
+	}
+
+	@Override
+	public String getUsername() {
+		return this.email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
 }
